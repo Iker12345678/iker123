beb0fff1c2bbc3c927517cce102674ad77758bff
Iker12345678 <151519140+Iker12345678@users.noreply.github.com>
2024-02-02 12:56:43 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("1a modificació Git");
+        System.out.println("2a modificació Git");
+
 
 
 
